38e9dbf8553e23ec0a6e12acc2105b36ccb3a61d
Luiz Henrique <luizhf42@gmail.com>
2023-07-01 17:28:56 -0300

    feat: create book detail component

---
+++ components/Book/Detail.vue
@@ -0,0 +1,22 @@
+<template>
+	<article>
+		<slot />
+		<span>{{ info }}</span>
+	</article>
+</template>
+
+<script setup lang="ts">
+defineProps({
+	info: String,
+});
+</script>
+
+<style lang="postcss" scoped>
+article {
+	@apply w-28 flex flex-col gap-1 items-center;
+
+	span, svg {
+		@apply text-gray-800 text-sm;
+	}
+}
+</style>
