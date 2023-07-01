1e4a7c1647f7129400e3585fdc303ab0835f6077
Luiz Henrique <luizhf42@gmail.com>
2023-07-01 17:29:11 -0300

    feat: create book details component

---
+++ components/Book/Details.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="details">
+		<BookDetail :info="isbn">
+			<BarcodeIcon title="ISBN 13" />
+		</BookDetail>
+		<BookDetail :info="pageCount + ' pages'">
+			<BookOpenPageVariantIcon title="Page count" />
+		</BookDetail>
+		<BookDetail :info="getCountryName(country)">
+			<EarthIcon title="Country" />
+		</BookDetail>
+	</div>
+</template>
+
+<script setup lang="ts">
+import BarcodeIcon from "vue-material-design-icons/Barcode.vue";
+import BookOpenPageVariantIcon from "vue-material-design-icons/BookOpenPageVariant.vue";
+import EarthIcon from "vue-material-design-icons/Earth.vue";
+
+const { country, isbn, pageCount } = defineProps({
+	country: String,
+	isbn: String,
+	pageCount: Number,
+});
+</script>
+
+<style lang="postcss" scoped></style>
