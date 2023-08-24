333869bbf3b0561d9adb0ffe42b80063ce3271ba
luizhf42 <luizhf42@gmail.com>
2023-08-24 18:37:26 -0300

    feat: create User model with user interfaces

---
+++ models/User.ts
@@ -0,0 +1,12 @@
+export interface User {
+    id: string;
+    name: string;
+    username: string;
+    picture: string;
+}
+
+export interface GoogleUserData {
+    sub: string;
+    name: string;
+    picture: string;
+}
