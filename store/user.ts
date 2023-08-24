afead55c9e27c4069c731ecde84744be9637d3da
luizhf42 <luizhf42@gmail.com>
2023-08-24 18:37:35 -0300

    feat: create user store

---
+++ store/user.ts
@@ -0,0 +1,13 @@
+import {defineStore} from 'pinia'
+import {fetchUserInfo} from "~/utils/fetchUserInfo";
+import {User} from "~/models/User";
+
+export const useUserStore = defineStore('user', () => {
+    const user = ref<User>()
+
+    const setUserInfo = async (id: string) => {
+        user.value = await fetchUserInfo(id) as User;
+    }
+
+    return {user, setUserInfo}
+})
