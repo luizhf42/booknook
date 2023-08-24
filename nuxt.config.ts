9fb10b4c122b27f78c3c4390b67c4b921478d20c
luizhf42 <luizhf42@gmail.com>
2023-08-24 18:36:50 -0300

    chore: install pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-	modules: ["@nuxtjs/tailwindcss"],
-	app: {
-		head: {
-			link: [
-				{ rel: "preconnect", href: "https://fonts.googleapis.com" },
-				{
-					rel: "preconnect",
-					href: "https://fonts.gstatic.com",
-					crossorigin: "",
-				},
-				{
-					rel: "stylesheet",
-					href: "https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700&display=swap",
-				},
-			],
-		},
-	},
+    modules: ["@nuxtjs/tailwindcss", "@pinia/nuxt"],
+    app: {
+        head: {
+            link: [
+                {rel: "preconnect", href: "https://fonts.googleapis.com"},
+                {
+                    rel: "preconnect",
+                    href: "https://fonts.gstatic.com",
+                    crossorigin: "",
+                },
+                {
+                    rel: "stylesheet",
+                    href: "https://fonts.googleapis.com/css2?family=Outfit:wght@400;600;700&display=swap",
+                },
+            ],
+            title: "BookNook 📚️"
+        },
+    },
 });
